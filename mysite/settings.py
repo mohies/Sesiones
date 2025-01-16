350ef11e544f91c4bda6ff8af22a303f83640d70
Mohcen Benizza <mohcen.benizza.bouzid.al@iespoligonosur.org>
2025-01-16 10:12:06 +0100

    para desplegar

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,12 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+import os
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'),True)
+env = environ.Env()
+SECRET_KEY =  env("SECRET_KEY")
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b3pfn(rkn18i*#asttftf3%d@99d5m5p6%dwoqwi12r(y9q28a'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
