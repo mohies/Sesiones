4a2356de753e6582c8459c434b1f02806abf3971
mohies <mohcen.benizza.bouzid.al@iespoligonosur.org>
2025-02-22 18:49:34 +0100

    hecho

--- torneo/api_urls.py
+++ torneo/api_urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('equipos/buscar/avanzado/', api_views.equipo_buscar_avanzado, name='equipo_buscar_avanzado'),
     path('participantes/buscar/avanzado/', api_views.participante_buscar_avanzado, name='participante_buscar_avanzado'),
     path('juegos/buscar/avanzado/', api_views.juego_buscar_avanzado, name='juego_buscar_avanzado'),
+    
     path('categorias/', api_views.categoria_list, name='categoria_list'),
     path('torneos/crear/', api_views.torneo_create, name='torneo_create'),
     path('torneos/editar/<int:torneo_id>/', api_views.torneo_editar, name='torneo_editar'),
@@ -30,12 +31,16 @@ urlpatterns = [
     path('torneos/actualizar-nombre/<int:torneo_id>/', api_views.torneo_actualizar_nombre, name='torneo_actualizar_nombre'),
     path('torneos/eliminar/<int:torneo_id>/', api_views.torneo_eliminar, name='torneo_eliminar'),
     path('torneos_list/', api_views.torneo_list, name='torneo_list'),
+    path('torneos/actualizar-imagen/<int:torneo_id>/', api_views.torneo_actualizar_imagen, name='torneo_actualizar_imagen'),
+    path('torneos/eliminar-imagen/<int:torneo_id>/', api_views.torneo_eliminar_imagen, name='torneo_eliminar_imagen'),
+    
     path('consolas/', api_views.consola_list, name='consola_list'),
     path('juegos/crear/', api_views.juego_create, name='juego_create'),
     path('juegos/<int:juego_id>/', api_views.juego_obtener, name='juego_obtener'),
     path('juegos/editar/<int:juego_id>/', api_views.juego_editar, name='juego_editar'),
     path('juegos/actualizar-nombre/<int:juego_id>/', api_views.juego_actualizar_nombre, name='juego_actualizar_nombre'),
     path('juegos/eliminar/<int:juego_id>/', api_views.juego_eliminar, name='juego_eliminar'),
+    
     path('usuarios/', api_views.usuario_list, name='usuario_list'),
     path('equipos/', api_views.equipo_list, name='equipo_list'),
     path('participantes/crear/', api_views.participante_create, name='participante_create'),
@@ -43,14 +48,14 @@ urlpatterns = [
     path('participantes/editar/<int:participante_id>/', api_views.participante_editar, name='participante_editar'),
     path('participantes/actualizar-equipos/<int:participante_id>/', api_views.participante_actualizar_equipos, name='participante_actualizar_equipos'),
     path('participantes/eliminar/<int:participante_id>/', api_views.participante_eliminar, name='participante_eliminar'),
-    path('usuarios-login/', api_views.usuariologin_list, name='usuariologin_list'),  # La URL debe coincidir con la del helper
+    path('usuarios-login/', api_views.usuariologin_list, name='usuariologin_list'), 
+    # La URL debe coincidir con la del helper
     path('jugadores/crear/', api_views.jugador_create, name='jugador_crear'),
     path('jugadores/obtener/<int:jugador_id>/', api_views.jugador_obtener, name='jugador_obtener'),
     path('jugadores/editar/<int:jugador_id>/', api_views.jugador_editar, name='jugador_editar'),
     path('jugadores/actualizar_puntos/<int:jugador_id>/', api_views.jugador_actualizar_puntos, name="jugador_actualizar_puntos"),
     path('jugadores/eliminar/<int:jugador_id>/<int:torneo_id>/', api_views.jugador_eliminar_torneo, name="jugador_eliminar_torneo"),
-    path('torneos/actualizar-imagen/<int:torneo_id>/', api_views.torneo_actualizar_imagen, name='torneo_actualizar_imagen'),
-    path('torneos/eliminar-imagen/<int:torneo_id>/', api_views.torneo_eliminar_imagen, name='torneo_eliminar_imagen'),
+
 
     
 
